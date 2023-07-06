9d60324c667612332fb5f1b0b74b7deb40920e99
Reza <mahmudreza1999@gmail.com>
2023-07-06 12:40:57 +0530

    completed unions

---
+++ myUnion.js
@@ -0,0 +1,11 @@
+let marks; //unions are basically used for adding more types to a variable.
+marks = 23;
+marks = "34";
+let Reza = { name: "Reza", id: 432 };
+Reza = { username: "Rz", id: 234 };
+function giveId(value) {
+    console.log(value);
+}
+giveId("12");
+giveId(87);
+// export {};
--- myUnion.ts
+++ myUnion.ts
@@ -1,3 +1,29 @@
 let marks:number | string; //unions are basically used for adding more types to a variable.
 marks = 23;
 marks = "34";
+
+type User = {
+    name:string;
+    id:number;
+}
+type Admin = {
+    username:string;
+    id:number;
+}
+let Reza: User | Admin = {name:"Reza",id:432}
+Reza = {username:"Rz",id:234}
+
+function giveId(value:number | string){
+    console.log(value);
+}
+giveId("12");
+giveId(87);
+
+
+//Arrays:
+// let data1: string[] | number[] = [1,2,"3"] gives error
+// let data1:(string | number)[] = [1,2,"3"] does not give error
+
+// let time:230 = 230; more strict
+//tsc.cmd --target es6 --module es6 myUnion.ts
+export {}
